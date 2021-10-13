d1fa4cb28fa7f74879903b5c6d907d7886975c2b
Celine86 <celine.obringer@live.fr>
2021-10-13 23:53:43 +0200

    Add new pointer

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 function moveDot(reddot){ 
     reddot.style.position ='absolute'; 
-    reddot.style.top = Math.floor(Math.random()*90+5)+'%'; 
-    reddot.style.left = Math.floor(Math.random()*90+5)+'%'; 
-} 
-    var movingdiv = document.querySelector('#movingdiv'); 
-    movingdiv.addEventListener('mouseenter', function(e){ 
-        moveDot(e.target);
-    });
-    var clickme = document.querySelector('#clickme');
-    clickme.addEventListener('click', function(e){
-        document.getElementById('congrats').textContent = `You nailed it !`;
-    });
+    reddot.style.top = Math.floor(Math.random()*80+5)+'%'; 
+    reddot.style.left = Math.floor(Math.random()*80+5)+'%'; 
+};
+
+let movingdiv = document.querySelector('#movingdiv'); 
+movingdiv.addEventListener('mouseenter', function(e){ 
+    moveDot(e.target);
+});
+
+let clickme = document.querySelector('#clickme');
+clickme.addEventListener('click', function(e){
+    document.getElementById('congrats').textContent = `You nailed it !`;
+});
+
+
+let mouseCursor = document.querySelector(".cursor");
+window.addEventListener('mousemove', cursor);
+
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + "px";
+    mouseCursor.style.left = e.pageX + "px";
+}
