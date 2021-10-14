75df4ebf2d23617f25319c7e7508906e8532ce51
Celine86 <celine.obringer@live.fr>
2021-10-14 17:48:11 +0200

    Change cursor because doens't work well with custom cursor

--- js/script.js
+++ js/script.js
@@ -4,21 +4,16 @@ function moveDot(reddot){
     reddot.style.left = Math.floor(Math.random()*80+5)+'%'; 
 };
 
-let movingdiv = document.querySelector('#movingdiv'); 
-movingdiv.addEventListener('mouseenter', function(e){ 
+document.querySelector('#movingdiv').addEventListener('mouseenter', function(e){ 
     moveDot(e.target);
 });
 
-let clickme = document.querySelector('#clickme');
-clickme.addEventListener('click', function(e){
+document.querySelector('#clickme').addEventListener('click', function(e){
     document.getElementById('congrats').textContent = `You nailed it !`;
 });
 
-
-let mouseCursor = document.querySelector(".cursor");
 window.addEventListener('mousemove', cursor);
-
 function cursor(e) {
-    mouseCursor.style.top = e.pageY + "px";
-    mouseCursor.style.left = e.pageX + "px";
+    document.querySelector(".cursor").style.top = e.pageY + "px";
+    document.querySelector(".cursor").style.left = e.pageX + "px";
 }
